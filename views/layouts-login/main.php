a21c5f5a6c9bf31854a57c370e569d76fad37d1e
alhamdulillah1234578 <kelvinrohmatsetiaji@gmail.com>
2023-06-05 20:11:09 +0700

    new

--- views/layouts-login/main.php
+++ views/layouts-login/main.php
@@ -61,5 +61,21 @@ LoginAsset::register($this);
     <?php endif ?>
 </script>
 
+<script>
+    const buttons = document.querySelectorAll('.button')
+    buttons.forEach(btn => {
+        btn.addEventListener('click', function(e) {
+
+            let x = e.clientX - e.target.offsetLeft;
+            let y = e.clientY - e.target.offsetTop;
+
+            let ripples = document.createElement('span');
+            ripples.style.left = x + 'px';
+            ripples.style.top = y + 'px';
+            this.appendChild(ripples);
+        })
+    }) 
+</script>
+
 </html>
 <?php $this->endPage() ?>
